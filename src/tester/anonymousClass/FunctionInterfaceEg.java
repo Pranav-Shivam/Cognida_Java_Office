15692b34dc05b5e30d676c3460e55fd374e0dd0f
Pranav-Shivam <21MCA2618@cuchd.in>
2023-02-10 10:40:36 +0530

    Sixth Commit

---
+++ src/tester/anonymousClass/FunctionInterfaceEg.java
@@ -0,0 +1,14 @@
+package tester.anonymousClass;
+
+public class FunctionInterfaceEg {
+    public static void main(String[] args) {
+        //use of functional interface using anonymous class
+        functionInterface1 x=new functionInterface1() {
+            @Override
+            public void sayHi() {
+                System.out.println("Jai Shree Ram");
+            }
+        };
+        x.sayHi();
+    }
+}
---
+++ src/tester/anonymousClass/functionInterface1.java
@@ -0,0 +1,7 @@
+package tester.anonymousClass;
+
+@FunctionalInterface
+public interface functionInterface1 {
+         public abstract void sayHi();
+
+}
